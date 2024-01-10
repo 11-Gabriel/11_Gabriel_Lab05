ab3c9b56094b80b2cf3f778f1ea206faa38859e3
Gabriel <gabwong9999@gmail.com>
2024-01-10 12:15:31 +0800

    Added Lab05 by Gabriel

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody playerRigidbody;
+    public int Coins;
+    private int TotalCoins;
+    public Text coinText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TotalCoins = GameObject.FindGameObjectsWithTag("Coins").Length;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(MoveHorizontal, 0, MoveVertical);
+        transform.Translate(movement * Time.deltaTime * speed);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Hazard")
+        {
+            SceneManager.LoadScene("Game Over");
+        }
+
+        if (collision.gameObject.tag == "Coin")
+        {
+            Coins++;
+            Destroy(collision.gameObject);
+            if (Coins == TotalCoins)
+            {
+                SceneManager.LoadScene("Level 2");
+            }
+        }
+    }
+
+    
+}
